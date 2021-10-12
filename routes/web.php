8020135c435ac77445fe6464b7bd67ebd7d365c2
KhaerunisaTrisania <haerkhaerunisa@gmail.com>
2021-10-12 14:18:43 +0700

    sesi 2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,48 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    echo "Hello World";
+});
+
+Route::get('/name/{name}', function ($name) {
+    return "Hello " . $name;
+});
+
+Route::get('/data/{data?}', function ($data = "Kosong") {
+    return "Isi Parameter = " . $data;
+});
+
+Route::get('/name/{name}', function ($name) {
+    return "Hello " . $name;
+})->where('name', '[A-Za-z]+');
+
+Route::get('/mahasiswa/{nrp}/{nama}', function ($nrp, $nama) {
+    return $nrp . " " . $nama;
+})->where([
+    'name' => '[A-Za-a]+',
+    'nrp' => '[0-9]+',
+]);
+
+Route::get('/cekbilangan/{bilangan}', function ($bilangan)  {
+    if($bilangan % 2 == 0){
+        return  "Bilangan genap";
+    }else{
+        return "Bilangan ganjil";
+}
+});
+
+Route::get('/cekbilangan/{bilangan}', function ($bilangan)  {
+    for($i = 0 ; $i <= $bilangan; $i++){
+    if($bilangan % 2 == 0){
+        echo  "{$i} genap";
+        echo "</br>";
+    }else{
+        echo "{$i} ganjil";
+        echo "</br>";
+}
+}
+});
+
+
